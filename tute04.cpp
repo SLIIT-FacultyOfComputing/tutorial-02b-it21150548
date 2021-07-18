62c0b0cd3245999a9d12e7bde9dad16ad833ba65
it21150548 Liyanage <87602577+it21150548@users.noreply.github.com>
2021-07-18 11:27:50 +0000

    Changed commands from C to C++ format

--- tute04.cpp
+++ tute04.cpp
@@ -27,3 +27,16 @@ int main() {
   std::cout << std::endl;
   return 0;
 }
+long Factorial(int no)
+{
+  long fac;
+  fac = 1;
+    for (int r=no; r >= 1; r--) {
+        fac = fac * r;
+    }
+    return fac;
+}
+long nCr(int n, int r)
+{
+  
+}
